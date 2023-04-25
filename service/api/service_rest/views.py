4949243ae3f1a791dd58421c11206523fdeda01b
Colin Prise <colinprise@MacBook-Air-3.local>
2023-04-24 19:27:08 -0700

    encoders

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from common.json import ModelEncoder
+from .models import AutomobileVO, Technician, Appointment
 
-# Create your views here.
+class AutomobileVOEncoder(ModelEncoder):
+    model = AutomobileVO
+    properties =[
+        'vin'
+    ]
+
+class TechnicianEncoder(ModelEncoder):
+    model = Technician
+    properties =[
+        'first_name',
+        'last_name',
+        'employee_id',
+    ]
+
+class AppointmentEncoder(ModelEncoder):
+    model = Appointment
+    properties = [
+        'date_time',
+        'reason',
+        'status',
+        'vin',
+        'customer',
+        'technician'
+    ]
+    encoders= {
+        "technician": TechnicianEncoder
+    }
