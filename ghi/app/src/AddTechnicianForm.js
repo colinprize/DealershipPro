642574ff42a9db22b40bfeb8e841458720f226ff
Colin Prise <colinprise@MacBook-Air-3.local>
2023-04-25 10:40:47 -0700

    form

--- ghi/app/src/AddTechnicianForm.js
+++ ghi/app/src/AddTechnicianForm.js
@@ -6,6 +6,37 @@ function TechnicianForm() {
     const [last_name, setLastName] = useState("");
     const [employee_id, setEmployeeID ] = useState("");
 
+    const handleFirstNameChange = (event) => {
+        const value = event.target.value;
+        setFirstName(value);
+    }
+
+    const handleLastNameChange = (event) => {
+        const value = event.target.value;
+        setLastName(value);
+    }
+
+    const handleEmployeedIDChange = (event) => {
+        const value = event.target.value;
+        setEmployeeID(value);
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDeafult();
+        const data = {}
+        data.first_name = first_name;
+        data.last_name = last_name;
+        data.employee_id = employee_id;
+
+        const url ="http://localhost:8080/api/technicians/";
+        const fetchConfig = {
+            method: "post",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }
+    }
 
     return(
 
