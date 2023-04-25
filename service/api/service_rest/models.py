d39547b26a5e104395448daa469d9f21975581e7
Colin Prise <colinprise@MacBook-Air-3.local>
2023-04-24 19:04:42 -0700

    few chnges

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class AutomobileVO(models.Model):
     vin = models.CharField(max_length=17, unique=True)
 
+    def __str__(self):
+        return self.vin
+
+
 class Technician(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -18,5 +22,5 @@ class Appointment(models.Model):
     technician = models.ForeignKey(
         Technician,
         related_name = "appointment",
-    on_delete = models.CASCADE,
+        on_delete = models.PROTECT,
     )
