32f73005edeebe555b1ac72ba2f56d766166c9a0
Colin Prise <colinprise@MacBook-Air-3.local>
2023-04-25 10:34:45 -0700

    form

--- ghi/app/src/AddTechnicianForm.js
+++ ghi/app/src/AddTechnicianForm.js
@@ -2,7 +2,13 @@ import React from "react";
 import { useState } from "react"
 
 function TechnicianForm() {
+    const [first_name, setFirstName] = useState("");
+    const [last_name, setLastName] = useState("");
+    const [employee_id, setEmployeeID ] = useState("");
 
 
+    return(
+
+    )
 }
 export default TechnicianForm
