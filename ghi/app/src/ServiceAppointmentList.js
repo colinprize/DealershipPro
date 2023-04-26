4132ed6ae3e5e2c041bc09a4361f9a61181db7cc
Colin Prise <colinprise@MacBook-Air-3.local>
2023-04-26 10:39:01 -0700

    front end is visible

--- ghi/app/src/ServiceAppointmentList.js
+++ ghi/app/src/ServiceAppointmentList.js
@@ -1,17 +1,59 @@
 import React, { useEffect, useState  } from 'react';
 
 function ServiceAppointmentList () {
-    const [service, setService] = useState([])
+    const [appointments, setAppointments] = useState([])
 
     const fetchData = async () => {
         const response = await fetch("http://localhost:8080/api/appointments/");
 
         if(response.ok) {
             const data = await response.json();
-            setService(data.appointments);
+            setAppointments(data.appointments);
         }
 
     }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Service Appointments</h1>
+            <table className='table table-striped'>
+            <thead>
+                <tr>
+                    <th>Vin</th>
+                    <th>Customer</th>
+                    {/* <th>Date</th> */}
+                    {/* <th>Time</th> */}
+                    <th>Technician</th>
+                    <th>Reason</th>
+                </tr>
+            </thead>
+            <tbody>
+                {appointments.map(appointment => {
+                    return (
+                        <tr key={appointment.vin} value={appointment.vin}>
+                            <td>{appointment.vin}</td>
+                            <td>{appointment.customer}</td>
+                            {/* <td>{appointment.date_time}</td> */}
+                            {/* <td>{appointment.date_time}</td> */}
+                            <td>{appointment.technician}</td>
+                            <td>{appointment.reason}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+        </div>
+    )
+
+
+
+
+
+
 }
 
 export default ServiceAppointmentList
