5ab700616766836f3633de6309ed67a749c8d0ca
Colin Prise <colinprise@MacBook-Air-3.local>
2023-04-26 09:45:05 -0700

    working on appointmentlist

--- ghi/app/src/ServiceAppointmentList.js
+++ ghi/app/src/ServiceAppointmentList.js
@@ -1,5 +1,17 @@
-function ServiceAppointmentList () {
+import React, { useEffect, useState  } from 'react';
 
+function ServiceAppointmentList () {
+    const [service, setService] = useState([])
+
+    const fetchData = async () => {
+        const response = await fetch("http://localhost:8080/api/appointments/");
+
+        if(response.ok) {
+            const data = await response.json();
+            setService(data.appointments);
+        }
+
+    }
 }
 
 export default ServiceAppointmentList
